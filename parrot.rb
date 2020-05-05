1d1ca24f92e0ea26c26dc3d4c809c48d015cd9cb
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-05 17:15:53 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(string = "Squawk!",string2 = "birdbird")
-  puts "#{string2}"
+def parrot(string = "Squawk!")
   return "#{string}"
 end
